971763d1cc87bb6f8add5709f65d7004d3dfb2d1
Todd Wolfson <todd@twolfson.com>
2014-10-12 23:02:54 +0900

    Handling installation annoyances

--- Vagrantfile
+++ Vagrantfile
@@ -7,6 +7,15 @@ Vagrant.configure("2") do |config|
   config.vm.box = "precise64"
   config.vm.box_url = "http://files.vagrantup.com/precise64.box"
 
+  # Install common packages
+  $install_common_packages = <<SCRIPT
+  if ! which curl; then
+    sudo apt-get install -y curl git
+  fi
+SCRIPT
+  config.vm.provision "shell", inline: $install_common_packages
+
+
   # Install test dependencies
   config.vm.provision "shell", path: "test/install.sh"
 end
--- test/install.sh
+++ test/install.sh
@@ -3,9 +3,18 @@
 set -e
 set -x
 
+# Update apt-get once
+if ! test -f .updated_apt_get; then
+  sudo apt-get update
+  touch .updated_apt_get
+fi
+
 # Install `node` for testing
 if ! which node > /dev/null; then
-  sudo apt-get install -y node
+  sudo apt-get install -y python-software-properties python g++ make
+  sudo add-apt-repository -y ppa:chris-lea/node.js
+  sudo apt-get update
+  sudo apt-get install -y nodejs
 fi
 
 # Install `n`, `node@0.8`, and `node@0.10`
