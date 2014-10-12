ee454e16f359932f759a1522cf17beb99b537d5e
Todd Wolfson <todd@twolfson.com>
2014-10-12 22:19:17 +0900

    Starting on script

---
+++ lib/fix-travis-ci.bash
@@ -0,0 +1,7 @@
+# Echo outputs and exit on first error
+set -e
+set -x
+
+# Determine if we are in `node@0.8`
+# TODO: cat into python via a docblock
+# TODO: Start testing via Vagrant so we don't affect the host environment
--- lib/fix-travis-ci.sh
+++
@@ -1,5 +0,0 @@
-function fix_travis_ci() {
-
-}
-
-module.exports = fix_travis_ci;
