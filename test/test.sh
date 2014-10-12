d2adacad42af4bd5dac5bfb6eae52b8d326e2f3f
Todd Wolfson <todd@twolfson.com>
2014-10-12 23:03:11 +0900

    Starting on tests

--- test/test.sh
+++ test/test.sh
@@ -3,5 +3,14 @@
 set -e
 set -x
 
-# Run our test command
-echo "hai"
+# In a `node@0.8` environment
+n use 0.8.28
+
+  # Save original npm version
+  npm_node_0_8_28_version="$(npm --version)"
+
+  # when `fix-travis-ci` runs
+  ../lib/fix-travis-ci.sh
+
+    # it upgrades `npm` to the latest one
+    test "$(npm --version)" != "$npm_node_0_8_28_version" || echo '`is_on_git`; $? != 0 in git directory' 1>&2
