a9c83d0c4ff2d6d86191a768100739e3bdb1d069
Todd Wolfson <todd@twolfson.com>
2014-10-12 23:12:07 +0900

    Completed gist of test suite

--- test/install.sh
+++ test/install.sh
@@ -17,9 +17,8 @@ if ! which node > /dev/null; then
   sudo apt-get install -y nodejs
 fi
 
-# Install `n`, `node@0.8`, and `node@0.10`
+# Install `n` and `node@0.8`
 if ! which n > /dev/null; then
   sudo npm install -g n
   n 0.8.28
-  n 0.10.32
 fi
--- test/test.sh
+++ test/test.sh
@@ -3,14 +3,52 @@
 set -e
 set -x
 
+# Generate assertion statements
+assert_equal () {
+  actual="$0"
+  expected="$1"
+  message="$2"
+  if test "$actual" != "$expected"; then
+    echo "$message" 1>&2
+    exit 1
+  fi
+}
+assert_not_equal () {
+  actual="$0"
+  expected="$1"
+  message="$2"
+  if test "$actual" == "$expected"; then
+    echo "$message" 1>&2
+    exit 1
+  fi
+}
+
+# Save root directory for navigating between fixtures
+# (e.g. fix-travis-ci)
+root_dir="$PWD"
+
 # In a `node@0.8` environment
 n use 0.8.28
+cd "$root_dir/test/test-files/node.travis.yml"
+
+  # BEFORE: Save original npm version
+  npm_version="$(npm --version)"
+
+  # when `fix-travis-ci` runs
+  "$root_dir/lib/fix-travis-ci.sh"
+
+    # it upgrades `npm` to the latest one
+    assert_not_equal "$(npm --version)" "$npm_version" '\`fix-travis-ci\` did not upgrade \`npm\` in a \`node@0.8\` environment'
+
+# In a non-node environment
+n use 0.10.32
+cd "$root_dir/test/test-files/node.travis.yml"
 
   # Save original npm version
-  npm_node_0_8_28_version="$(npm --version)"
+  npm_version="$(npm --version)"
 
   # when `fix-travis-ci` runs
   ../lib/fix-travis-ci.sh
 
-    # it upgrades `npm` to the latest one
-    test "$(npm --version)" != "$npm_node_0_8_28_version" || echo '`is_on_git`; $? != 0 in git directory' 1>&2
+    # it doesn't touch `npm`
+    assert_equal "$(npm --version)" "$npm_version" '\`fix-travis-ci\` upgraded \`npm\` in a non-node environment'
