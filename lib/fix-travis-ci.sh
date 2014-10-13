ee74f0f88d4a7f15ae348dc83e0f9706f42b842c
Todd Wolfson <todd@twolfson.com>
2014-10-13 19:02:27 +0900

    Attempting to start executing Python via CLI

--- lib/fix-travis-ci.sh
+++ lib/fix-travis-ci.sh
@@ -6,6 +6,9 @@ set -x
 # Determine if we are in `node@0.8`
 # TODO: cat into python via a docblock
 # TODO: Start testing via Vagrant so we don't affect the host environment
+cat | python - <<EOF
+  print 'hello world'
+EOF
 
 # TODO: Find `language`, look for `lib/fixes/{{language}}.sh`
 #   If it exists, run it. Inside of `lib/fixes/node.sh`, implement `npm` adjustments
