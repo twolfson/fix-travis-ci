e7a8d7ce4fbc9b6b5339eb47f633f1c1c5de2297
Todd Wolfson <todd@twolfson.com>
2014-10-13 19:15:25 +0900

    Starting to change plan

--- lib/fix-travis-ci.sh
+++ lib/fix-travis-ci.sh
@@ -4,18 +4,16 @@ set -e
 set -x
 
 # Determine if we are in `node@0.8`
-# TODO: cat into python via a docblock
 # TODO: Start testing via Vagrant so we don't affect the host environment
-python - <<EOF
-# Load in dependencies
-import sys
+# TODO: Figure out why Vagrant
+# TODO:
+# python - <<EOF
+# # Load in dependencies
+# import yaml
 
-# Start our script
-print 'hello world'
-sys.exit()
-
-
-EOF
+# # Start our script
+# print 'hello world'
+# EOF
 
 # TODO: Find `language`, look for `lib/fixes/{{language}}.sh`
 #   If it exists, run it. Inside of `lib/fixes/node.sh`, implement `npm` adjustments
