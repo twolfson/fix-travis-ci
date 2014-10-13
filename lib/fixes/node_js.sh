c4cc419b395d960e03e4e3ec3f232bf773c42c81
Todd Wolfson <todd@twolfson.com>
2014-10-13 19:28:19 +0900

    Starting on node fix

---
+++ lib/fixes/node_js.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Echo outputs and exit on first error
+set -e
+set -x
+
+# If we are on an early version of `npm`, upgrade to the latest 1.x.x
+# DEV: `caret` notation is supported by `1.4.3` but `npm` didn't start using it until 2.x.x
