93e5f744a80ad76e33b0a4399a419a5b779004dd
Todd Wolfson <todd@twolfson.com>
2014-10-17 20:06:19 -0700

    Removing all the sudos

--- test/install.sh
+++ test/install.sh
@@ -5,21 +5,21 @@ set -x
 
 # Update apt-get once
 if ! test -f .updated_apt_get; then
-  sudo apt-get update
+  apt-get update
   touch .updated_apt_get
 fi
 
 # Install `node` for testing
 if ! which node > /dev/null; then
-  sudo apt-get install -y python-software-properties python g++ make
-  sudo add-apt-repository -y ppa:chris-lea/node.js
-  sudo apt-get update
-  sudo apt-get install -y nodejs
+  apt-get install -y python-software-properties python g++ make
+  add-apt-repository -y ppa:chris-lea/node.js
+  apt-get update
+  apt-get install -y nodejs
 fi
 
 # Install `n`, `node@0.8`, and `node@0.10`
 if ! which n > /dev/null; then
-  sudo npm install -g n
-  sudo n 0.8.28
-  sudo n 0.10.32
+  npm install -g n
+  n 0.8.28
+  n 0.10.32
 fi
